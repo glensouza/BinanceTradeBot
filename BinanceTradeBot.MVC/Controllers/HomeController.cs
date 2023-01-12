393185ff9ba9fef27c59bfff5f6bd9cdcd9da93e
Glen Souza <glenster75@gmail.com>
2023-01-12 12:43:02 -0800

    Set Up Strategy Files

---
+++ BinanceTradeBot.MVC/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using BinanceTradeBot.MVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Microsoft.Identity.Web;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using BinanceTradeBot.Common;
+
+namespace BinanceTradeBot.MVC.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly GraphProfileClient _graphProfileClient;
+        private readonly ITokenAcquisition _tokenAcquisition;
+        private readonly GraphServiceClient _graphClient;
+
+        public string UserDisplayName { get; private set; } = string.Empty;
+        public string UserPhoto { get; private set; }
+
+        public HomeController(ILogger<HomeController> logger, GraphProfileClient graphProfileClient, ITokenAcquisition tokenAcquisition, GraphServiceClient graphClient)
+        {
+            this._logger = logger;
+            this._graphProfileClient = graphProfileClient;
+            this._graphClient = graphClient;
+        }
+
+        public async Task OnGetAsync()
+        {
+            // Acquire the access token.
+            string[] scopes = new string[] { "user.read" };
+            string accessToken = await this._tokenAcquisition.GetAccessTokenForUserAsync(scopes);
+            this._logger.LogInformation($"Token: {accessToken}");
+
+            // Use the access token to call a protected web API.
+            HttpClient client = new();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            string json = await client.GetStringAsync("https://graph.microsoft.com/v1.0/me?$select=displayName");
+            this._logger.LogInformation(json);
+
+            User user = await this._graphProfileClient.GetUserProfile();
+            this.UserDisplayName = user.DisplayName.Split(' ')[0];
+            this.UserPhoto = await this._graphProfileClient.GetUserProfileImage();
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            using StrategyMovingAverage strategy = new();
+            await strategy.RunStrategyAsync();
+            return this.View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return this.View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BinanceTradeBot.MVC/Models/GraphApiHelper.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Claims;
+using Microsoft.Identity.Client;
+
+namespace BinanceTradeBot.MVC.Models
+{
+    public class GraphApiHelper
+    {
+        private string appId;
+        private string redirectUri;
+        private string appSecret;
+        private string[] graphScopes;
+
+        public GraphApiHelper()
+        {
+        }
+
+        public async Task<string> GetUserAccessTokenAsync()
+        {
+            IConfidentialClientApplication? idClient = ConfidentialClientApplicationBuilder.Create(this.appId)
+                .WithRedirectUri(this.redirectUri)
+                .WithClientSecret(this.appSecret)
+                .Build();
+
+            //var tokenStore = new SessionTokenStore(idClient.UserTokenCache, HttpContext.Current, ClaimsPrincipal.Current);
+
+            IEnumerable<IAccount>? accounts = await idClient.GetAccountsAsync();
+
+            // By calling this here, the token can be refreshed
+            // if it's expired right before the Graph call is made
+            AuthenticationResult? result = await idClient
+                .AcquireTokenSilent(this.graphScopes, accounts.FirstOrDefault())
+                .ExecuteAsync();
+
+            return result.AccessToken;
+        }
+    }
+}
